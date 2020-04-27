b06f7f727d6117e7c77bf97729a6c811f5a729a0
Kevin C Malin <racmalin@yahoo.com>
2020-04-27 03:28:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,14 @@ def sum_array(array)
   sum
 end 
 
-def add_s
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
 
 
