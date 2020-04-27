d725ffb1019d7c4b4a0690d0846146200ed99433
Kevin C Malin <racmalin@yahoo.com>
2020-04-27 03:37:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,42 +2,42 @@ def sort_array_asc(num)
   num = [25, 7, 1]
   num.sort
 end
-  
+end 
 def sort_array_desc(num)
   num = [25, 7, 14]
   num.sort! {|x, y| y <=> x}
 end
-
+end
 def sort_array_char_count(array)
   array = ["cat", "dogs", "Horses"]
   array.sort_by(&:length)
 end
-
+end
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
-
+end
 def reverse_array(array)
   array.reverse
 end
-
+end
 def kesha_maker(array)
   array.each do |item|
     item[2] = "$"
 end
-
+end
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
 end
-
+end
 def sum_array(array)
   sum = 0
   array.each do |num|
     sum += num
-  # end
-  # sum
+  end
+  sum
 end 
 
 def add_s(array)
@@ -46,7 +46,7 @@ def add_s(array)
       word
     else
       word + "s"
-    # end
+    end
 end
 
 
